929fa5ec0dcc6f1dc5ac730680588a4e72c31245
amir-sediqi69 <amir.sediqi69@gmail.com>
2021-08-12 15:32:12 +0200

    room app

---
+++ app/src/main/java/com/example/roomwordsample/Word.java
@@ -0,0 +1,20 @@
+package com.example.roomwordsample;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {this.mWord = word;}
+ // Every field that's stored in the database needs to be either public or have a "getter" method.
+ // This sample provides a getWord() method.
+    public String getWord(){return this.mWord;}
+}
